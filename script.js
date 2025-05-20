416c8d9ce4b535aaa817bfeb2fdc260d56527e46
Matteda Ramesh <85834506+rummy143679@users.noreply.github.com>
2025-05-20 12:11:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+function onClickStart(e){
+	alert("click")
+	e.preventDefault();
+	const ip1 = document.getElementById("player-1");
+	const ip2 = document.getElementById("player-2");
+	const player1 = "";
+	const player2 = "";
+	if(ip1.value != "" || ip2.value != ""){
+		player1 = ip1.value;
+		player2 = ip2.value;
+		
+	}else{
+		alert("one player name is missing");
+	}
+	
+	
+}
